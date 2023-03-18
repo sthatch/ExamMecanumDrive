e4b9f8e7e2900095c7e6af9672b9a97488132dbb
sthatch <steven.tyler777@gmail.com>
2023-03-18 13:22:58 -0400

    Implement Talon Controllers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,29 +4,46 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
-  private static final int kFrontLeftChannel = 2;
-  private static final int kRearLeftChannel = 3;
-  private static final int kFrontRightChannel = 1;
-  private static final int kRearRightChannel = 0;
+  private static final int kFrontLeftFrontChannel = 4;
+  private static final int kRearLeftFrontChannel = 6;
+  private static final int kFrontRightFrontChannel = 0;
+  private static final int kRearRightFrontChannel = 2;
+
+  private static final int kFrontLeftRearChannel = 5;
+  private static final int kRearLeftRearChannel = 7;
+  private static final int kFrontRightRearChannel = 1;
+  private static final int kRearRightRearChannel = 3;
 
   private static final int kJoystickChannel = 0;
 
   private MecanumDrive m_robotDrive;
-  private Joystick m_stick;
+  private XboxController m_stick;
 
   @Override
   public void robotInit() {
-    PWMSparkMax frontLeft = new PWMSparkMax(kFrontLeftChannel);
-    PWMSparkMax rearLeft = new PWMSparkMax(kRearLeftChannel);
-    PWMSparkMax frontRight = new PWMSparkMax(kFrontRightChannel);
-    PWMSparkMax rearRight = new PWMSparkMax(kRearRightChannel);
+    Talon frontLeftFront = new Talon(kFrontLeftFrontChannel);
+    Talon frontLeftRear = new Talon(kFrontLeftRearChannel);
+    MotorControllerGroup frontLeft = new MotorControllerGroup(frontLeftFront,frontLeftRear);
+
+    Talon rearLeftFront = new Talon(kRearLeftFrontChannel);
+    Talon rearLeftRear = new Talon(kRearLeftRearChannel);
+    MotorControllerGroup rearLeft = new MotorControllerGroup(rearLeftFront, rearLeftRear);
+
+    Talon frontRightFront = new Talon(kFrontRightFrontChannel);
+    Talon frontRightRear = new Talon(kFrontRightRearChannel);
+    MotorControllerGroup frontRight = new MotorControllerGroup(frontRightFront, frontRightRear);
+
+    Talon rearRightFront = new Talon(kRearRightFrontChannel);
+    Talon rearRightRear = new Talon(kRearRightRearChannel);
+    MotorControllerGroup rearRight = new MotorControllerGroup(rearRightFront, rearRightRear);
 
     // Invert the right side motors.
     // You may need to change or remove this to match your robot.
@@ -35,13 +52,13 @@ public class Robot extends TimedRobot {
 
     m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-    m_stick = new Joystick(kJoystickChannel);
+    m_stick = new XboxController(kJoystickChannel);
   }
 
   @Override
   public void teleopPeriodic() {
     // Use the joystick X axis for forward movement, Y axis for lateral
     // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    m_robotDrive.driveCartesian(-m_stick.getLeftY(), -m_stick.getLeftX(), -m_stick.getRightX());
   }
 }
